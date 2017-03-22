491a0b7820f0684ba3137be9a6368d6a0e8b07db
Adjokip <Adjokip@users.noreply.github.com>
2017-03-22 04:16:50 +0000

    Some validation to upload.php
    Check if the user is logged in and check the file extensions.
    
    PR for: http://www.opengamepanel.org/forum/viewthread.php?thread_id=5289#post_25844

--- modules/news/upload.php
+++ modules/news/upload.php
@@ -4,33 +4,59 @@
 // Check http://www.netartmedia.net/newslister for demos and information
 // Released under the MIT license
 ?><?php
+
+session_name('opengamepanel_web');
+session_start();
+
 $output_dir = "uploads/";
 if(isset($_FILES["myfile"]))
 {
 	$ret = array();
 
-	$error =$_FILES["myfile"]["error"];
- 
-	if(!is_array($_FILES["myfile"]["name"])) 
-	{
-	
- 	 	$fileName = $_FILES["myfile"]["name"];
- 		move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir.$fileName);
-    	$ret[]= $fileName;
-		
+	// Is user logged in?
+	if(isset($_SESSION['users_login'])) {
+
+		$allowedExtensions = array('png', 'gif', 'jpg', 'jpeg');
+
+		if(!is_array($_FILES["myfile"]["name"])) 
+		{
+			$fileName = $_FILES["myfile"]["name"];
+			$ext = end(explode('.', $fileName));
+
+			if (in_array($ext, $allowedExtensions)) {
+				move_uploaded_file($_FILES["myfile"]["tmp_name"], $output_dir.$fileName);
+				$ret[]= $fileName;
+
+			} else {
+				// something with a non-image extension was posted.
+				// log?
+			}
+
+		} else {
+			
+			$fileCount = count($_FILES["myfile"]["name"]);
+
+			for($i=0; $i < $fileCount; $i++) {
+
+				$fileName = $_FILES["myfile"]["name"][$i];
+				$ext = end(explode('.', $fileName));
+
+				if (in_array($ext, $allowedExtensions)) {
+					move_uploaded_file($_FILES["myfile"]["tmp_name"][$i], $output_dir.$fileName);
+					$ret[]= $fileName;
+
+				} else {
+					// something with a non-image extension was posted.
+					// log?
+				}
+			}
+		}
+
+	} else {
+		// User isn't logged in.
+		// Log something?
 	}
-	else  
-	{
-	
-	  $fileCount = count($_FILES["myfile"]["name"]);
-	  for($i=0; $i < $fileCount; $i++)
-	  {
-		$fileName = $_FILES["myfile"]["name"][$i];
-		move_uploaded_file($_FILES["myfile"]["tmp_name"][$i],$output_dir.$fileName);
-	  	$ret[]= $fileName;
-	  }
-	
-	}
-    echo json_encode($ret);
+
+	echo json_encode($ret);
  }
  ?>
