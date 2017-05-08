a52bee27259d9c1ff20e30afed2c57e311c261d1
®omano <omano@redorchestra2.fr>
2017-05-08 16:54:29 +0200

    TinyMCE editor div fix

--- modules/news/module.php
+++ modules/news/module.php
@@ -24,7 +24,7 @@
 
 // Module general information
 $module_title = "NewsLister";
-$module_version = "0.6";
+$module_version = "0.7";
 $db_version = 0;
 $module_required = FALSE;
 $module_menus = array( array( 'subpage' => 'news', 'name'=>'News', 'group'=>'user' ),
